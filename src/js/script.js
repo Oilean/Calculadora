ecf2d913facdfc1f8b51e80bac83637e8132f8ae
William <william.santos935@gmail.com>
2022-09-21 03:49:03 -0300

    commit

--- src/js/script.js
+++ src/js/script.js
@@ -4,7 +4,7 @@ const dark = document.getElementById('calculadora')
 const rodape = document.getElementById('rodape')
 
 const toggle = document.getElementById('toggle');
-toggle.onclick = function(){
+toggle.onclick = function () {
     toggle.classList.toggle('ativado');
     body.classList.toggle('ativado');
     dark.classList.toggle('ativado');
@@ -13,26 +13,26 @@ toggle.onclick = function(){
 
 
 // calculadora
-function insert(num){
+function insert(num) {
     var numero = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = numero + num;
 }
 
-function clean(){
+function clean() {
     document.getElementById('resultado').innerHTML = '';
 }
 
-function back(){
+function back() {
     var resultado = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length - 1);
 }
 
-function calcular(){
+function calcular() {
     var resultado = document.getElementById('resultado').innerHTML;
-    if(resultado){
+    if (resultado) {
         document.getElementById('resultado').innerHTML = eval(resultado);
 
-    }else{
+    } else {
         document.getElementById('resultado').innerHTML = "Error"
     }
 }
